b093b896a381224d78575b38c2f84787befa19a4
Max Zheltikov <maxcruer@gmail.com>
2021-07-09 22:07:24 +0300

    little refactor & reformat

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 
 namespace Zheltikov\Memoize;
 
-use Exception;
+use function Zheltikov\Invariant\invariant;
 
 final class Config
 {
@@ -20,31 +20,33 @@ final class Config
 	 */
 	public static function getHashAlgo(): string
 	{
-		if (static::$hash_algo === null) {
+		if (self::$hash_algo === null) {
 			// FIXME: the md5 hash algo may have collisions but for now, it'll suffice
 			// By default we use the md5 hashing algorithm
-			static::$hash_algo = 'md5';
+			self::$hash_algo = 'md5';
 		}
 
-		return static::$hash_algo;
+		return self::$hash_algo;
 	}
 
-	/**
-	 * @param string|null $hash_algo
-	 * @return void
-	 * @throws \Exception
-	 */
+    /**
+     * @param string|null $hash_algo
+     * @return void
+     * @throws \Zheltikov\Invariant\InvariantException
+     */
 	public static function setHashAlgo(?string $hash_algo = null): void
 	{
 		if ($hash_algo === null) {
-			static::$hash_algo = null;
+			self::$hash_algo = null;
 			return;
 		}
 
-		if (!in_array($hash_algo, hash_algos())) {
-			throw new Exception('Invalid hashing algorithm supplied: ' . var_export($hash_algo, true));
-		}
+		invariant(
+		    in_array($hash_algo, hash_algos(), true),
+            'Supplied value %s must be valid hashing algorithm',
+            $hash_algo
+        );
 
-		static::$hash_algo = $hash_algo;
+		self::$hash_algo = $hash_algo;
 	}
 }
