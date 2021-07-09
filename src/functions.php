1a94c70a8ba34a26c6050cae2c60b67e241d56f0
Max Zheltikov <maxcruer@gmail.com>
2021-07-09 12:44:37 +0300

    when switching the used hash algo, clear the cache

--- src/functions.php
+++ src/functions.php
@@ -10,12 +10,25 @@ function wrap(callable $fn): callable
 {
 	return function () use ($fn) {
 		static $cache = [];
+		static $hash_algo = null;
+
+		if ($hash_algo === null) {
+			// Initialize the hash algo
+			$hash_algo = Config::getHashAlgo();
+		} else {
+			// when switching the used hash algo, clear the cache, thus, freeing memory
+			// The hash algo has changed
+			if ($hash_algo !== Config::getHashAlgo()) {
+				// Clear the cache
+				$cache = [];
+				$hash_algo = Config::getHashAlgo();
+			}
+		}
 
 		$args = func_get_args();
+		$key = hash($hash_algo, serialize($args));
 
-		// TODO: when switching the used hash algo, clear the cache, thus, freeing memory
-		$key = hash(Config::getHashAlgo(), serialize($args));
-
+		// There is no such parameter combo in the cache, compute the result and cache it
 		if (!array_key_exists($key, $cache)) {
 			$cache[$key] = call_user_func_array($fn, $args);
 		}
