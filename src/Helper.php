54f54a74ec39040da834f7b8cef74c42b9bdde50
Max Zheltikov <maxcruer@gmail.com>
2021-08-22 21:29:36 +0300

    made a nicer interface for users

--- src/Helper.php
+++ src/Helper.php
@@ -2,6 +2,9 @@
 
 namespace Zheltikov\Memoize;
 
+use function Zheltikov\Memoize\_Private\caller_to_string;
+use function Zheltikov\Memoize\_Private\get_user_caller;
+
 /**
  * TODO: remove, the technically-required, `static` argument to all these methods, this way its usage is nicer :)
  *
@@ -16,50 +19,56 @@ namespace Zheltikov\Memoize;
 trait Helper
 {
     /**
-     * @param MemoizedCallable|null $static
+     * @var \Zheltikov\Memoize\MemoizedCallable[]
+     */
+    protected static array $__memoize_lsb_storage__ = [];
+
+    /**
      * @param callable $fn
      * @param ...$arguments
      * @return mixed
      * @throws \Zheltikov\Exceptions\InvariantException
+     * @deprecated Use the `wrap()` function instead.
      */
-    public static function memoize(?MemoizedCallable &$static, callable $fn, ...$arguments)
+    final protected static function memoize(callable $fn, ...$arguments)
     {
-        if ($static === null) {
-            $static = wrap($fn);
-        }
-
-        return $static->call(...$arguments);
+        return wrap($fn)->call(...$arguments);
     }
 
     /**
-     * @param string $classname
-     * @param MemoizedCallable|null $static
      * @param callable $fn
      * @param ...$arguments
      * @return mixed
      * @throws \Zheltikov\Exceptions\InvariantException
      */
-    public static function memoizeLSB(
-        string $classname,
-        ?MemoizedCallable &$static,
+    final protected static function memoizeLSB(
         callable $fn,
         ...$arguments
     ) {
-        if ($static === null) {
-            $static = wrap(
-                function () use ($fn): MemoizedCallable {
-                    /** @var MemoizedCallable|null $fn2 */
-                    static $fn2 = null;
+        return static::memoizeLSBOptions($fn, null, null, ...$arguments);
+    }
 
-                    if ($fn2 === null) {
-                        $fn2 = wrap($fn);
-                    }
-
-                    return $fn2;
-                }
-            );
+    /**
+     * @param callable $fn
+     * @param \Zheltikov\Memoize\Cache|null $cache
+     * @param \Zheltikov\Memoize\KeyGenerator|null $key_generator
+     * @param ...$arguments
+     * @return mixed
+     * @throws \Zheltikov\Exceptions\InvariantException
+     */
+    final protected static function memoizeLSBOptions(
+        callable $fn,
+        ?Cache $cache = null,
+        ?KeyGenerator $key_generator = null,
+        ...$arguments
+    ) {
+        $caller = caller_to_string(get_user_caller());
+        if (array_key_exists($caller, static::$__memoize_lsb_storage__)) {
+            $callable = static::$__memoize_lsb_storage__[$caller];
+        } else {
+            $callable = new MemoizedCallable($fn, $cache, $key_generator);
+            static::$__memoize_lsb_storage__[$caller] = $callable;
         }
-
-        return $static->call($classname)->call(...$arguments);
+        return $callable->call(...$arguments);
     }
 }
